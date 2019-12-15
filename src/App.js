3b11b990d6c7bf9d5d11901bd2cdda1ac6d3880d
Kevin Yen <ky.17364@gmail.com>
2019-12-14 16:15:23 -0800

    Create logo.js component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Logo from './logo.js';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo onClick={() => setCount(count+1)}></Logo>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>You clicked {count} times</p>
         <a
           className="App-link"
           href="https://reactjs.org"
